94a9d16fb71265631cb958ca8ac8b97587c623e7
Trevor <trevorgloe@gmail.com>
2023-05-17 12:56:53 -0700

    made it work with current circuit

---
+++ project2/analyze_data.py
@@ -0,0 +1,53 @@
+### Script to read and analyze gyroscope and accelerometer data read from the SD card, produced by the arduino
+# Written for project 2, Physics 357 Spring 2023 with Dr. Fernsler
+# Data is stored in csv file called DATA.TXT - contains accelerometer data in 2 dimensions (x and y) and angular rates in z
+# 
+# 
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# read data from csv
+data = pd.read_csv('DATA.TXT')
+print(data)
+
+# extract data into numpy arrays
+tstr = data['time'].to_numpy()
+axstr = data['AcX'].to_numpy()
+aystr = data['AcY'].to_numpy()
+gzstr = data['GyZ'].to_numpy()
+
+# truncate the data to avoid weird stuff and cast as floats
+t = tstr[2:].astype(float)
+ax = axstr[2:].astype(float)
+ay = aystr[2:].astype(float)
+gz = gzstr[2:].astype(float)
+print(ax)
+
+# integrate the gryo data just for fun
+int_ang = np.zeros(gz.shape)
+for idx in range(len(t)):
+    int_ang[idx] = np.trapz(gz[:idx],t[:idx])
+
+
+fig_accel = plt.figure()
+
+plt.scatter(t,ax,color='r',label='x-axis')
+plt.scatter(t,ay,color='b',label='y-axis')
+plt.xlabel('Time (ms)')
+plt.ylabel('Acceleration')
+plt.legend()
+plt.xticks(np.arange(np.min(t),np.max(t),5000))
+
+fig_gyr = plt.figure()
+plt.scatter(t,gz,color='r')
+plt.xlabel('Time (ms)')
+plt.ylabel('Angular rate')
+
+fig_ang = plt.figure()
+plt.scatter(t,int_ang,color='b')
+plt.xlabel('Time (ms)')
+plt.ylabel('Angle integrated from gyro data')
+
+plt.show()
--- project2/readwrite_gyro_accel_data/readwrite_gyro_accel_data.ino
+++ project2/readwrite_gyro_accel_data/readwrite_gyro_accel_data.ino
@@ -1,9 +1,111 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SD.h>
+#include<Wire.h>
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
+
+void setup()
+
+{
+
+  Serial.begin(9600);
+
+  pinMode(10, OUTPUT); //10 is the critical pin for SD card shield
+
+  SD.begin(10);
+
+  pinMode(3, OUTPUT);
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+
+  File out;
+  out = SD.open("data.txt", FILE_WRITE);
+  out.println("time,AcX,AcY,GyZ"); //print header of text file for reference
+  out.close();
+
+  pinMode(7, INPUT); // initialize this pin to read the switch position
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+
+void loop()
+
+{
+
+  //int switch;
+
+  Serial.println(digitalRead(7));
+  
+  // if switch is turned, read data. the switch value is stored in the state of pin 7
+  if (digitalRead(7) == HIGH) {
+
+    File out;
+
+    unsigned long t;
+
+    t = millis();
+
+    Serial.println(t);
+
+    //Serial.print(",");
+
+    //Serial.println(n);
+
+    //file names must use 8.3 convention (8 character name, 3 character extension)
+
+    out = SD.open("data.txt", FILE_WRITE);
+
+    if (out)
+
+    {
+
+      Wire.beginTransmission(MPU_addr);
+      Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+      Wire.endTransmission(false);
+      Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+      AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+      AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+      AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+      Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+      GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+      GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+      GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+      Serial.print("AcX = "); Serial.print(AcX);
+      Serial.print(" | AcY = "); Serial.print(AcY);
+      Serial.print(" | GyZ = "); Serial.println(GyZ);
+
+      out.print(t);
+
+      out.print(",");
+
+      //out.println(n);
+      out.print(AcX);
+      out.print(",");
+      out.print(AcY);
+      out.print(",");
+      out.println(GyZ);
+
+      out.close();
+
+      digitalWrite(3, HIGH);
+
+      delay(5);
+
+      digitalWrite(3, LOW);
+
+      //delay(10);
+
+    }
+
+    delay(2);
+
+  } else{
+    delay(2);
+  }
+
 
 }
